c3dfd297d0592ba873dd68104ec804fff5073622
NAINAYUNIZA14 <nainayuniza29@gmail.com>
2024-06-03 09:23:17 +0700

    TUGAS 1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -3,9 +3,9 @@ class Contoh1 extends CI_Controller
 {
     public function index()
     {
-    echo "<h1>Perkenalkan</h1>";
-    echo"Nama saya NAINA YUNIZA
-        Saya tingga di TANJUN RAYA 2
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya NAINA YUNIZA
+        Saya tingga di TANJUNG RAYA 2
         olah raga yang saya sukai adalah
         bola volly";
     }

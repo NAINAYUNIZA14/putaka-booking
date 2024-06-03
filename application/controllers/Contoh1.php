72de3ea49ca1b8392e9bdb2347c94a2f6a685dff
NAINAYUNIZA14 <nainayuniza29@gmail.com>
2024-06-03 08:24:15 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya NAINA YUNIZA
+        Saya tingga di TANJUN RAYA 2
+        olah raga yang saya sukai adalah
+        bola volly";
+    }
+}
